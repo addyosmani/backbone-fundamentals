81bef97992389b86c014801ee048e3c85d6d89a8
orizens <farhioren@gmail.com>
2013-01-27 22:44:43 +0200

    updated todo with latest 0.9.10
    updated backbone, underscore, localstorage, listeners.
    the app gets a collection as a data source

--- practicals/modular-todo-app/js/main.js
+++ practicals/modular-todo-app/js/main.js
@@ -6,12 +6,30 @@ require.config({
   paths: {
     jquery: 'libs/jquery/jquery-min',
     underscore: 'libs/underscore/underscore-min',
-    backbone: 'libs/backbone/backbone-optamd3-min',
+    backbone: 'libs/backbone/backbone',
+
+    // storage has built in support for requirejs
+    // hence, it doesn't need to configured in 'shim'
+    storage: 'libs/backbone/backbone.localStorage',
     text: 'libs/require/text'
+  },
+
+  shim: {
+
+    underscore: {
+      exports: '_'
+    },
+
+    backbone: {
+      deps: [ 'underscore', 'jquery' ],
+      exports: 'Backbone'
+    }
   }
 
 });
 
-require(['views/app'], function(AppView){
-  var app_view = new AppView;
+require([ 'views/app', 'collections/todos' ], function(AppView, AppCollection){
+  var app_view = new AppView({
+    collection: AppCollection
+  });
 });
